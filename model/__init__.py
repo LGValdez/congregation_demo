4f7b0bd23a25e4a5b4ad36e06136acfd287ad3a2
Gabriel Valdez <gabriel.valdez@jalasoft.com>
2020-06-09 10:35:26 -0400

    [GVP] remove files no longer used

--- model/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import holy_gift
-from . import holy_ministry
-from . import res_congregation
-from . import ministerial_project
-from . import school_program
-from . import participation_registry
-from . import congregation_partner
